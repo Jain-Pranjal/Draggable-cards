3d37ea8da9fd075d5815b2dd0f59bcd9ec191c0f
Pranjal Jain <pranjalworkon@gmail.com>
2024-05-20 11:25:44 +0530

    making the foreground

---
+++ components/Forground.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+const Forground = () => {
+  return (
+
+      <div className="fixed z-[2]  top-0 left-0  w-full h-full ">
+        <Card/>
+      </div>
+  )
+}
+
+export default Forground
